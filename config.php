b3e50dbd9f74b21e33da4851307256486bb75d13
joshva-A <87659018+joshva-A@users.noreply.github.com>
2021-08-18 20:18:01 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 	$database = "world_bank";
 	// Create a database connection
 	$conn = mysqli_connect($server, $username, $password, $database);
-	//$conn = mysqli_connect('shareddb-x.hosting.stackcp.net','shivangi','12345678@','malyaraaj-313537e38d');
+	
 
 	if(!$conn){
 		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
